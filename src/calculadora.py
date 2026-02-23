7cfdce2834bfb882a32d0c1167b418a27f7f313a
Rodrigo Victor Beltrão <150438774+Rodibre8708@users.noreply.github.com>
2026-02-23 14:41:29 +0000

    teste

--- src/calculadora.py
+++ src/calculadora.py
@@ -6,5 +6,5 @@ def subtrai(a, b):
 
 def divide(a, b):
     if b == 0:
-        raise ValueError("Não é possível dividir por zero")
+        raise ValueError("Não é possível dividir por zero.")
     return a / b
